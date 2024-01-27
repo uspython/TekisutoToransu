8d4cb986f21caddeb26ad5cbf932d06ed83a9a83
JeffZhao <jeff40577@gmail.com>
2024-01-27 17:46:45 +0900

    add camera buttons

--- src/Views/CameraPage.tsx
+++ src/Views/CameraPage.tsx
@@ -1,11 +1,14 @@
+import {useAppState} from '@react-native-community/hooks';
+import {useIsFocused} from '@react-navigation/native';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
   useColorScheme,
 } from 'react-native';
-import React, {useEffect} from 'react';
 import {
   Camera,
   useCameraDevice,
@@ -14,10 +17,57 @@ import {
 
 export default function CameraPage(props) {
   const {navigation} = props;
-  const isDarkMode = useColorScheme() === 'dark';
+
+  const camera = useRef<Camera>(null);
+  const [showGrid, setShowGrid] = useState(false);
+
+  const isFocused = useIsFocused();
+  const appState = useAppState();
+  const isActive = isFocused && appState === 'active';
+
   const {hasPermission, requestPermission} = useCameraPermission();
+  const isDarkMode = useColorScheme() === 'dark';
 
   const device = useCameraDevice('back');
+
+  const toggleGrid = useCallback(() => {
+    setShowGrid(!showGrid);
+  }, [showGrid]);
+
+  const takePic = useCallback(async () => {
+    const photo = await camera.current?.takePhoto({
+      qualityPrioritization: 'speed',
+      flash: 'off',
+      enableShutterSound: false,
+    });
+  }, []);
+
+  const renderGrid = () => {
+    if (!showGrid) {
+      return null;
+    }
+    return (
+      <View style={styles.gridContainer}>
+        {Array.from({length: 2}).map((_, index) => (
+          <React.Fragment key={`line-${index}`}>
+            <View
+              style={[
+                styles.gridLine,
+                {top: `${(index + 1) * 33.33}%`}, // for horizontal lines
+              ]}
+            />
+            <View
+              style={[
+                styles.gridLine,
+                {left: `${(index + 1) * 33.33}%`}, // for vertical lines
+                {height: '100%', width: 1}, // Adjust the dimensions for vertical lines
+              ]}
+            />
+          </React.Fragment>
+        ))}
+      </View>
+    );
+  };
 
   useEffect(() => {
     async function grantPermission() {
@@ -34,8 +84,87 @@ export default function CameraPage(props) {
   }
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+    <SafeAreaView style={styles.container}>
+      <Camera
+        ref={camera}
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={isActive}
+        photo={true}
+      />
+      {renderGrid()}
+      {/* Top Buttons */}
+      <View style={styles.topButtonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            /* Flash functionality */
+          }}>
+          <Text>Flash</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={toggleGrid}>
+          <Text>Grid</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Bottom Buttons */}
+      <View style={styles.bottomButtonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.goBack()}>
+          <Text>Back</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={takePic}>
+          <Text>Shot</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.goBack()}>
+          <Text>Gallery</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  topButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingTop: 10,
+  },
+  bottomButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 15,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: 'white',
+    borderRadius: 5,
+  },
+  gridContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    flex: 1,
+  },
+  gridLine: {
+    position: 'absolute',
+    borderColor: 'white',
+    borderWidth: 1,
+    width: '100%',
+  },
+});
